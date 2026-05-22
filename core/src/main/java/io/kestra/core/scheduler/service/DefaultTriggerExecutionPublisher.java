bf590ce696e2a33f84599e564bd0ed2b4616c4fd
Roman Acevedo <roman.acevedo62@gmail.com>
2026-05-22 11:02:19 +0200

    fix(scheduler): remove auditlogging from DefaultTriggerExecutionPublisher

--- core/src/main/java/io/kestra/core/scheduler/service/DefaultTriggerExecutionPublisher.java
+++ core/src/main/java/io/kestra/core/scheduler/service/DefaultTriggerExecutionPublisher.java
@@ -38,12 +38,10 @@ public class DefaultTriggerExecutionPublisher implements TriggerExecutionPublish
     public void send(final Execution execution) {
         try {
             this.executionCommandQueue.emit(toCreate(execution));
-            this.executionEventPublisher.publishEvent(CrudEvent.create(execution));// FIXME probably a bug, should not exist here
         } catch (QueueException e) {
             try {
                 Execution failedExecution = fail(execution, e);
                 this.executionCommandQueue.emit(toCreate(failedExecution).withStateType(State.Type.FAILED));
-                this.executionEventPublisher.publishEvent(CrudEvent.create(execution));// FIXME same
             } catch (QueueException ex) {
                 LOG.error("Unable to emit the execution", ex);
             }
